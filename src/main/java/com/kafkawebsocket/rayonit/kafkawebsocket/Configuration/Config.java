35acc6e7d30038ef4559da522f1cd5b91b24725a
MikeaMihali <mikea.mihali@fshnstudent.info>
2018-07-23 16:36:23 +0200

    cors

---
+++ src/main/java/com/kafkawebsocket/rayonit/kafkawebsocket/Configuration/Config.java
@@ -0,0 +1,18 @@
+package com.kafkawebsocket.rayonit.kafkawebsocket.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class Config extends WebMvcConfigurerAdapter {
+    @Bean
+    public WebMvcConfigurer corsConfigure(){
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
