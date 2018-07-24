632a15c283fa512d84fc0507da2438d693ade659
MikeaMihali <mikea.mihali@fshnstudent.info>
2018-07-24 10:49:24 +0200

    ada

--- src/main/java/com/kafkawebsocket/rayonit/kafkawebsocket/Controllers/Controller.java
+++ src/main/java/com/kafkawebsocket/rayonit/kafkawebsocket/Controllers/Controller.java
@@ -45,6 +45,16 @@ public class Controller {
             new Point(  12.309494018554688,
                     41.918628865183045)
     );
+//public static final Polygon USER_ZONE =new GeoJsonPolygon(
+//        new Point( 12.9473876953125,
+//                42.097203425683055),
+//        new Point(13.01605224609375,
+//                41.88592102814744),
+//        new Point(13.245391845703123,
+//                42.014611228817955),
+//        new Point(  12.9473876953125,
+//                42.097203425683055)
+//);
     @PostMapping("/update")
     public ResponseEntity getUpdates(@RequestBody EventResponseMainModel data){
         eventsRepository.save(data);
@@ -69,7 +79,8 @@ public class Controller {
       double[] southWest={minLat,minLng};
       double[] northEast={maxLat,maxLng};
       Box bounds=new Box(northEast,southWest);
-      return mongoTemplate.find(new Query(Criteria.where("data.0.geoData.0.geometry").within(bounds).within(USER_ZONE)),EventResponseMainModel.class);
+       System.out.println(bounds.toString());
+      return mongoTemplate.find(new Query(Criteria.where("data.0.geoData.0.geometry").within(USER_ZONE).andOperator(Criteria.where("data.0.geoData.0.geometry").within(bounds))),EventResponseMainModel.class);
 //       return eventsRepository.findByGeometryWithin(b);
 //       return eventsRepository.findAll();
     }
